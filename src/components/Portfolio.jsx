4583fd3e13a2533081f13084cc68d1a3fda6ee16
Kent Ball <kent.vincent.ball@gmail.com>
2024-09-29 17:54:45 -0400

    Updated portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,6 +9,24 @@ const Portfolio = () => {
       deployedLink: "https://tech-blog-95af.onrender.com/",
       githubLink: "https://github.com/digitalscribe53/Tech-Blog"
     },
+    {
+      title: "Book Reviewer App",
+      image: "/bookreviewer.png",
+      deployedLink: "https://book-reviewer-7tpk.onrender.com",
+      githubLink: "https://github.com/mirandarb/Book-Reviewer"
+    },
+    {
+      title: "Employee Tracker App",
+      image: "/employeetracker.png",
+      deployedLink: "https://drive.google.com/file/d/1lxiRq7z8vzTkgfTCElCj7e5kapEn9oA4/view",
+      githubLink: "https://github.com/digitalscribe53/Employee-Tracker"
+    },
+    {
+      title: "Progressive Web App Text Editor",
+      image: "/pwa-texteditor.JPG",
+      deployedLink: "https://pwa-text-editor-enhq.onrender.com/",
+      githubLink: "https://github.com/digitalscribe53/PWA-Text-Editor"
+    },
     // Add more projects here
   ];
 
