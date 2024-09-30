57e814425d1732308b5fcd82a4360d969a70e813
Kent Ball <kent.vincent.ball@gmail.com>
2024-09-30 12:49:51 -0400

    Updated project card style

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Project = ({ title, image, deployedLink, githubLink }) => {
   return (
     <div className="project">
-      <img src={image} alt={title} className="project-image" />
+      <div className="project-image-container">
+        <img src={image} alt={title} className="project-image" />
+      </div>
       <div className="project-details">
         <h3>{title}</h3>
         <div className="project-links">
