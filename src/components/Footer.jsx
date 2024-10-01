eced1f9d2206c7efc8fb90391c3bef512eb6283e
Kent Ball <kent.vincent.ball@gmail.com>
2024-10-01 12:38:42 -0400

    Fixed link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <a href="https://github.com/digitalscribe53" target="_blank" rel="noopener noreferrer">GitHub</a>
       <a href="https://linkedin.com/in/kent-ball" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="https://https://x.com/november1actual" target="_blank" rel="noopener noreferrer">X</a>
+      <a href="https://x.com/november1actual" target="_blank" rel="noopener noreferrer">X</a>
     </footer>
   );
 };
