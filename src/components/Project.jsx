6fb0278dcaf01a223b2b8c8fc1a85b781095ff49
Kent Ball <kent.vincent.ball@gmail.com>
2025-04-03 21:17:45 -0400

    Updated Project component to conditionally pass the deployedLink prop

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,8 +9,15 @@ const Project = ({ title, image, deployedLink, githubLink }) => {
       <div className="project-details">
         <h3>{title}</h3>
         <div className="project-links">
-          <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed App</a>
-          <a href={githubLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+          {/* Only render the Deployed App link if a deployedLink exists */}
+          {deployedLink && (
+            <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+              Deployed App
+            </a>
+          )}
+          <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            GitHub Repo
+          </a>
         </div>
       </div>
     </div>
