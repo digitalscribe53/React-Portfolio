59b0a176a59af40e138611181bfbac784e0888d0
Kent Ball <kent.vincent.ball@gmail.com>
2025-06-11 12:31:38 -0400

    Updated resume doc

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Resume = () => {
   const proficiencies = [
     "JavaScript(ES6+)", "HTML5", "CSS3", "React.js", "Node.js",
-    "Express.js", "MongoDB", "SQL and NoSQL", "RESTful APIs", "Apollo GraphQL", "MVC Architecture", "Git and Version Control"
+    "Express.js", "MongoDB", "SQL and NoSQL Databases", "Sequelize ORM", "RESTful APIs", "Apollo GraphQL", "MVC Architecture", "Git and Version Control"
   ];
 
   return (
     <section className="resume">
       <h2 className="page-title">Resume</h2>
       <div className="download-link">
-      <a href="/Kent-Ball-Resume-2024.pdf" download>Download Resume as PDF</a>
+      <a href="/Kent Ball Resume 2025.pdf" download>Download Resume as PDF</a>
       </div>
       <h3>Proficiencies</h3>
       <ul>
